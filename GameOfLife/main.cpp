901d29c01160f18b0dc6e5d9c91273afb2dad929
Joni Rajala <jonimatiasrajala@gmail.com>
2019-12-04 15:39:23 +0200

    Main arguments checking.

--- GameOfLife/main.cpp
+++ GameOfLife/main.cpp
@@ -8,17 +8,41 @@
 
 #include <iostream>
 #include <string>
+#include <climits>
 
 #include "CellBoard.hpp"
 #include "TerminalView.hpp"
 #include "CommandParser.hpp"
 
+
+bool isBoardSizeValid(long width, long height) {
+	
+	if (width == 0 || width == LONG_MAX || width == LONG_MIN) {
+		return false;
+	} else if (height == 0 || height == LONG_MAX || height == LONG_MIN) {
+		return false;
+	}
+	
+	return true;
+	
+}
+
+
 int main(int argc, const char * argv[]) {
 	
-	int width  = atoi(argv[1]);
-	int height = atoi(argv[2]);
+	if (argc != 3) {
+		std::cout << "Needs width and height as parameters." << std::endl;
+		return 1;
+	}
+	long width  = strtol(argv[1], nullptr, 10);
+	long height = strtol(argv[2], nullptr, 10);
 	
-	CellBoard board = CellBoard(width, height);
+	if (isBoardSizeValid(width, height) == false) {
+		std::cout << "Invalid width and height parameters." << std::endl;
+		return 1;
+	}
+	
+	CellBoard board = CellBoard((int)width, (int)height);
 	TerminalView view;
 	CommandParser parser;
 	board.randomize();
